04855fcec50e08b67cdfd5a5bdf0e783ce2c5998
Njengak11 <njengak11@gmail.com>
2021-10-06 10:20:12 +0300

    added stae management

--- App.js
+++ App.js
@@ -1,16 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, {useState} from 'react';
+import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Task from './components/Task';
+
 
 export default function App() {
+
+const [task,setTask] = useState();
+const [taskItems, setTaskItems] = useState([]);
+
+const handleAddTask = () => {
+  Keyboard.dismiss
+  setTaskItems([...taskItems,task])
+  setTask(null);
+}
+
+const completeTask = (index) => {
+  let itemsCopy = [...taskItems];
+  itemsCopy.splice(index, 1);
+  setTaskItems(itemsCopy)
+}
+
+
   return (
     <View style={styles.container}>
       <View style={styles.taskWrapper}>
         <Text style={styles.sectionTitle}>Today's Tasks</Text>
         <View style={styles.items}>
-          
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key={index}  onPress={() => completeTask(index)}>
+                  <Task text={item} /> 
+                </TouchableOpacity>
+              )
+            })
+          }
         </View>
       </View>
+
+      <KeyboardAvoidingView
+      behavior={Platform.OS === 'íos' ? 'padding':'height'}
+      style={styles.writeTextWrapper}
+      >
+        <TextInput 
+        style={styles.input}
+        placeholder={'Write a task'}
+        value={task}
+        onChangeText={text => setTask(text)}
+        />
+        <TouchableOpacity
+        onPress ={() => handleAddTask()}
+        >
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -28,6 +74,36 @@ sectionTitle:{
   fontSize:25,
   fontWeight:'bold'
 },
-items:{}
+items:{
+  marginTop:30
+},
+writeTextWrapper:{
+  position: 'absolute',
+    bottom: 20,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+},
+input:{
+  paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+},
+addWrapper:{
+  width: 60,
+  height: 60,
+  backgroundColor: '#FFF',
+  borderRadius: 60,
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderColor: '#C0C0C0',
+  borderWidth: 1,
+},
+addText:{},
 });
  
